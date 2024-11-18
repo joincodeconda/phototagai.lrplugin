7ed22e3f14dd7f156adbba1e6f8898d72fdc60f1
abarlev <aaron@codeconda.com>
2024-11-18 14:51:37 -0500

    Set plugin version in info file

--- Info.lua
+++ Info.lua
@@ -19,5 +19,5 @@ return {
             enabledWhen = "photosSelected",
         },
     },
-    VERSION = { major = 1, minor = 0, revision = 0 },
+    VERSION = { major = 1, minor = 1, revision = 2 },
 }
